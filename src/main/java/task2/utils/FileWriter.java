422b8b84b4fdc33e365ba0001c96181b4d5ff26d
Pavlov Igor <prozproz@mail.ru>
2020-03-31 12:52:06 +0300

    fixed method of writing to file

--- src/main/java/task2/utils/FileWriter.java
+++ src/main/java/task2/utils/FileWriter.java
@@ -3,8 +3,8 @@ package task2.utils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import task2.models.Composite;
@@ -13,26 +13,27 @@ import task2.services.Component;
 
 
 public class FileWriter {
-
+    String string = "";
+    File file = new File("result.txt");
 
     public void write(Component allText) {
-
         for (int i = 0; i < ((Composite) allText).getComponentList().size(); i++) {
             Component sentence = ((Composite) allText).getComponentList().get(i);
             List<Component> s = ((Composite) sentence).getComponentList();
             for (int j = 0; j < s.size(); j++) {
-                write(((Element) s.get(j)).getStr());
+                string += ((Element) s.get(j)).getStr();
             }
         }
-    }
 
-
-    public void write(String str) {
-        File file = new File("result.txt");
         try {
-            FileUtils.writeStringToFile(file, str, Charset.forName("UTF-8"));
+            FileUtils.writeStringToFile(file, string, StandardCharsets.UTF_8.name());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
+
+
