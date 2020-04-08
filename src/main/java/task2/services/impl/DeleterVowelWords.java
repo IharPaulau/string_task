dd9e2872b99386bd984b35cb0962d474bdb9fa5c
Pavlov Igor <prozproz@mail.ru>
2020-04-08 15:58:20 +0300

    refactoring DeleterConsonantWords

--- src/main/java/task2/services/impl/DeleterVowelWords.java
+++ src/main/java/task2/services/impl/DeleterVowelWords.java
@@ -14,22 +14,24 @@ public class DeleterVowelWords implements WordDeleter {
     private static final Logger LOGGER = Logger.getLogger(DeleterVowelWords.class);
     private static final String ANY_ELEVEN_LETTER_WORD_STARTING_WITH_CONSONANT = "[^аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouyAEIOUY\\s][a-zA-Zа-яА-Я]{%d}";
 
+    @Override
     public TextComponent deleter(TextComponent allText, int length) {
-        if(length <= 0){
+        TextComponent result = allText;
+        if (length <= 0) {
             LOGGER.info("wrong length value - the word length for deletion cannot be less than or equal to zero");
-            return allText;
-        }
-        String regex = String.format(ANY_ELEVEN_LETTER_WORD_STARTING_WITH_CONSONANT, length-1);
-        for (int i = 0; i < ((CompositeTextFragments)allText).getOneLevelFragments().size(); i++) {
-            TextComponent sentence = ((CompositeTextFragments)allText).getOneLevelFragments().get(i);
-            List<TextComponent> elements = ((CompositeTextFragments) sentence).getOneLevelFragments();
-            for (int indexElement = 0; indexElement < elements.size(); indexElement++) {
-                if (Pattern.matches(regex, ((MinTextFragment) elements.get(indexElement)).getTextElement())) {
-                    elements.remove(indexElement);
+        } else {
+            String regex = String.format(ANY_ELEVEN_LETTER_WORD_STARTING_WITH_CONSONANT, length - 1);
+            for (int i = 0; i < ((CompositeTextFragments) allText).getOneLevelFragments().size(); i++) {
+                TextComponent sentence = ((CompositeTextFragments) allText).getOneLevelFragments().get(i);
+                List<TextComponent> elements = ((CompositeTextFragments) sentence).getOneLevelFragments();
+                for (int indexElement = 0; indexElement < elements.size(); indexElement++) {
+                    if (Pattern.matches(regex, ((MinTextFragment) elements.get(indexElement)).getTextElement())) {
+                        elements.remove(indexElement);
+                    }
                 }
             }
+            LOGGER.info("all words in parsed text starting with a consonant and eleven letters long was deleted");
         }
-        LOGGER.info("all words in parsed text starting with a consonant and eleven letters long was deleted");
-        return allText;
+        return result;
     }
 }
